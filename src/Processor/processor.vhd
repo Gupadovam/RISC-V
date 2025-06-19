3262185fa0656ad2a53959c9b6b6757201daf6d6
GabrielSpdf <gb.spada@hotmail.com>
2025-06-19 01:50:05 -0300

    wip: refactorizing the project

--- src/Processor/processor.vhd
+++ src/Processor/processor.vhd
@@ -10,11 +10,11 @@ entity processor is
 end entity;
 
 architecture a_processor of processor is
-    -- Componentes externos
     component state_machine is
         port(
             clk : in std_logic;
             rst : in std_logic;
+            exception : in std_logic;
             estado : out unsigned(1 downto 0)
         );
     end component;
@@ -28,11 +28,21 @@ architecture a_processor of processor is
             jump_addr: in unsigned(7 downto 0); 
             br_en   : in std_logic; -- br_enable
             br_addr : in unsigned(7 downto 0); -- br_in
-            opcode : in unsigned(4 downto 0);
+            instruction : in unsigned(13 downto 0);
             beq_cond : in std_logic; -- zero flag
             blt_cond : in std_logic; -- carry flag
             bcs_cond : in std_logic; -- overflow flag
             data_out: out unsigned(7 downto 0) 
+        );
+    end component;
+
+    component mmu is
+        port (
+            ram_read_en  : in std_logic;
+            ram_write_en : in std_logic;
+            endereco_in  : in std_logic_vector(15 downto 0);
+            endereco_out : out std_logic_vector(15 downto 0);
+            exception    : out std_logic
         );
     end component;
 
@@ -44,26 +54,70 @@ architecture a_processor of processor is
         );
     end component;
 
+    component ram is
+        port (
+            clk      : in std_logic;
+            endereco : in unsigned(15 downto 0);
+            wr_en    : in std_logic;
+            dado_in  : in unsigned(15 downto 0);
+            dado_out : out unsigned(15 downto 0) 
+        );
+    end component;
+
     component control_unit is
         port(
             clk : in std_logic;
             instruction : in unsigned(13 downto 0);
             estado : in unsigned(1 downto 0);
+            jump_en : out std_logic;
+            jump_addr : out unsigned(7 downto 0);
             br_en : out std_logic;
             br_addr : out unsigned(7 downto 0);
             br_cond : out unsigned(4 downto 0);
             reg_wr_en : out std_logic;
             sel_op_ula : out std_logic_vector(1 downto 0);
-            source_reg : out unsigned(2 downto 0);
-            destiny_reg : out unsigned(2 downto 0);
-            jump_en : out std_logic;
-            jump_addr : out unsigned(7 downto 0);
+            sel_mux_regs : out std_logic;
             acc_en : out std_logic;
+            acc_ovwr_en : out std_logic;
+            acc_mux_sel : out std_logic;
+            rst_acc : out std_logic;
+            flags_wr_en : out std_logic;
             immediate : out unsigned(8 downto 0);
-            sel_acc_src : out std_logic;
-            borrow_in : out std_logic;
-            pc_en : out std_logic;
-            ir_en : out std_logic -- ir := instruction register
+            ram_wr_en : out std_logic;
+            ram_rd_en : out std_logic;
+            reg_code : out unsigned(2 downto 0)
+        );
+    end component;
+
+    component RegisterBank is
+        port(
+            clk      : in std_logic;
+            rst      : in std_logic;
+            wr_en    : in std_logic;                  
+            wr_sel   : in unsigned(2 downto 0); -- selec. reg escrito
+            rd_sel   : in unsigned(2 downto 0); -- selec. reg lido
+            data_in  : in unsigned(15 downto 0); 
+            data_out : out unsigned(15 downto 0)     
+        );
+    end component;
+
+    component Register1Bit is
+        port(
+            clock    : in std_logic;
+            rst      : in std_logic;
+            wr_en    : in std_logic;
+            data_in  : in std_logic;
+            data_out : out std_logic
+        );
+    end component;
+
+    component Register16Bits is
+        port(
+            clk : in std_logic;
+            rst : in std_logic;
+            wr_en : in std_logic;
+            data_in : in unsigned(15 downto 0);
+            data_out : out unsigned(15 downto 0)
         );
     end component;
 
@@ -81,59 +135,63 @@ architecture a_processor of processor is
         );
     end component;
 
-    component acumulador is
-        port (
-            clk      : in std_logic;
-            rst      : in std_logic;
-            wr_en    : in std_logic;
-            data_in  : in unsigned(15 downto 0);
-            data_out : out unsigned(15 downto 0)
-        );
-    end component;
-
-    component banco_reg is
-        port (
-            clk      : in std_logic;
-            rst      : in std_logic;
-            wr_en    : in std_logic;                  
-            wr_sel   : in unsigned(2 downto 0); -- selec. reg escrito
-            rd_sel   : in unsigned(2 downto 0); -- selec. reg lido
-            data_in  : in unsigned(15 downto 0); 
-            data_out : out unsigned(15 downto 0)     
-        );
-    end component;
-
-    component instruction_register is
-        port (
-            clk      : in std_logic;
-            rst      : in std_logic;
-            wr_en    : in std_logic;
-            data_in  : in unsigned(13 downto 0);
-            data_out : out unsigned(13 downto 0)
-        );
-    end component;
-
     -- Sinais
+    -- state machine
     signal estado_s : unsigned(1 downto 0);
-    signal pc_data_out_s : unsigned(7 downto 0);
+
+    -- pc
     signal pc_wr_en_s : std_logic;
+    signal pc_data_out_s : unsigned(7 downto 0);
+
+    -- mmu
+    signal mmu_exception_s : std_logic := '0';
+    signal mmu_endereco_s  : unsigned(15 downto 0) := (others => '0');
+
+    -- rom
     signal rom_data_out_s : unsigned(13 downto 0);
-    signal opcode_s : unsigned(4 downto 0);
-    signal jump_en_s : std_logic;
-    signal jump_addr_s : unsigned(7 downto 0);
-    signal reg_wr_en_s, acc_en_s : std_logic;
-    signal sel_op_ula_s : std_logic_vector(1 downto 0);
-    signal sel_acc_src_s : std_logic;
-    signal source_reg_s, destiny_reg_s : unsigned(2 downto 0);
-    signal immediate_s : unsigned(8 downto 0);
-    signal borrow_in_s : std_logic;
-    signal reg_data_out_s : unsigned(15 downto 0);
-    signal ula_in_b_s : unsigned(15 downto 0);
-    signal ula_result_s : unsigned(15 downto 0);
-    signal acc_data_out_s : unsigned(15 downto 0);
-    signal acc_input_s : unsigned(15 downto 0);
-    signal ir_data_out_s : unsigned(13 downto 0);
-    signal ir_en_s : std_logic;
+
+    -- ram
+    signal ram_data_out_s : unsigned(15 downto 0) := (others => '0');
+
+    -- control unit
+    signal cu_jump_en_s : std_logic := '0';
+    signal cu_jump_addr_s : unsigned(7 downto 0);
+    signal cu_br_en_s : std_logic := '0';
+    signal cu_br_addr_s : unsigned(7 downto 0) := (others => '0');
+    signal cu_br_cond_s  : unsigned(4 downto 0) := (others => '0');
+    signal cu_sel_op_ula_s : std_logic_vector(1 downto 0);
+    signal cu_sel_mux_regs_s : std_logic := '0';
+    signal cu_reg_wr_en_s : std_logic := '0';
+    signal cu_acc_en_s : std_logic := '0';
+    signal cu_acc_ovwr_en_s : std_logic := '0';
+    signal cu_acc_mux_sel_s : std_logic := '0';
+    signal cu_rst_acc_s : std_logic := '0';
+    signal cu_flags_wr_en_s : std_logic := '0';
+    signal cu_immediate_s : unsigned(8 downto 0);
+    signal cu_ram_wr_en_s : std_logic := '0';
+    signal cu_ram_rd_en_s : std_logic := '0';
+    signal cu_reg_code_s : unsigned(2 downto 0);
+
+    -- register bank
+    signal rb_reg_en_s : std_logic;
+    signal rb_data_wr_mux_s : unsigned(15 downto 0) := (others => '0');
+    signal rb_data_r_s : unsigned(15 downto 0) := (others => '0');
+
+    -- acc 
+    signal acc_en_s : std_logic := '0';
+    signal acc_out_s : unsigned(15 downto 0) := (others => '0');
+    signal acc_in_s : unsigned(15 downto 0) := (others => '0');
+
+    -- ula
+    signal ula_out_s : unsigned(15 downto 0) := (others => '0');
+    signal ula_carry : std_logic := '0';
+    signal ula_zero : std_logic := '0';
+    signal ula_negative : std_logic := '0';
+    signal ula_overflow : std_logic := '0';
+
+    -- flag registers
+    signal fr_zero_flag : std_logic := '0';
+    signal fr_carry_flag : std_logic := '0';
 
 begin
     -- Componentes principais
@@ -141,6 +199,7 @@ begin
         port map(
             clk => clk,
             rst => rst,
+            exception => mmu_exception_s,
             estado => estado_s
         );
 
@@ -149,9 +208,14 @@ begin
             clk => clk,
             rst => rst,
             wr_en => pc_wr_en_s,
-            jump_en => jump_en_s,
-            jump_addr => jump_addr_s,
-            opcode => opcode_s,
+            jump_en => cu_jump_en_s,
+            jump_addr => cu_jump_addr_s,
+            br_en => cu_br_en_s,
+            br_addr => cu_br_addr_s,
+            instruction => rom_data_out_s, -- opcode eh feito dentro do componente
+            beq_cond => fr_zero_flag,
+            blt_cond => fr_carry_flag,
+            bcs_cond => ula_negative,
             data_out => pc_data_out_s
         );
 
@@ -165,76 +229,91 @@ begin
     cu: control_unit
         port map(
             clk => clk,
-            instruction => ir_data_out_s,
+            instruction => rom_data_out_s, 
             estado => estado_s,
-		  br_en : out std_logic;
-		  br_addr : out unsigned(7 downto 0);
-		  br_cond : out unsigned(4 downto 0);
-            reg_wr_en => reg_wr_en_s,
-            sel_op_ula => sel_op_ula_s,
-            source_reg => source_reg_s,
-            destiny_reg => destiny_reg_s,
-            jump_en => jump_en_s,
-            jump_addr => jump_addr_s,
-            acc_en => acc_en_s,
-            immediate => immediate_s,
-            sel_acc_src => sel_acc_src_s,
-            borrow_in => borrow_in_s,
-            pc_en => pc_wr_en_s,
-            ir_en => ir_en_s
+            jump_en => cu_jump_en_s,
+            jump_addr => cu_jump_addr_s,
+            br_en => cu_br_en_s,
+            br_addr => cu_br_addr_s,
+            br_cond => cu_br_cond_s,
+            reg_wr_en => cu_reg_wr_en_s,
+            sel_op_ula => cu_sel_op_ula_s,
+            sel_mux_regs => cu_sel_mux_regs_s,
+            acc_en => cu_acc_en_s,
+            acc_ovwr_en => cu_acc_ovwr_en_s,
+            acc_mux_sel => cu_acc_mux_sel_s,
+            rst_acc => cu_rst_acc_s,
+            flags_wr_en => cu_flags_wr_en_s,
+            immediate => cu_immediate_s,
+            ram_wr_en => cu_ram_wr_en_s,
+            ram_rd_en => cu_ram_rd_en_s,
+            reg_code => cu_reg_code_s
         );
 
-    br: banco_reg
+    rb: RegisterBank
         port map(
             clk => clk,
             rst => rst,  
-            wr_en => reg_wr_en_s,
-            wr_sel => destiny_reg_s,
-            rd_sel => source_reg_s,
-            data_in => acc_data_out_s,
-            data_out => reg_data_out_s
+            wr_en => rb_reg_en_s,
+            wr_sel => cu_reg_code_s,
+            rd_sel => cu_reg_code_s,
+            data_in => rb_data_wr_mux_s,
+            data_out => rb_data_r_s
         );
 
-    ir: instruction_register
-        port map(
-            clk => clk,
-            rst => rst,
-            wr_en => ir_en_s,
-            data_in => rom_data_out_s,
-            data_out => ir_data_out_s
-        );
+    accumulator: Register16Bits port map(
+        clk => clk,
+        rst => rst,
+        wr_en => acc_en_s,
+        data_in => acc_in_s,
+        data_out => acc_out_s
+    );
 
     ula1: ula
         port map(
-            in_a => acc_data_out_s,
-            in_b => ula_in_b_s,
-            sel_op => sel_op_ula_s,
-            value_immediate => immediate_s,
-            borrow_in => borrow_in_s,
-            carry_out => open,
-            overflow => open,
-            zero => open,
-            negative => open,
-            result => ula_result_s
+            in_a => acc_out_s,
+            in_b => rb_data_r_s,
+            sel_op => cu_sel_op_ula_s,
+            value_immediate => cu_immediate_s,
+            borrow_in => ula_carry,
+            carry_out => ula_carry,
+            overflow => ula_overflow,
+            zero => ula_zero,
+            negative => ula_negative,
+            result => ula_out_s
         );
 
-    acc: acumulador
-        port map(
-            clk => clk,
-            rst => rst,
-            wr_en => acc_en_s,
-            data_in => acc_input_s,
-            data_out => acc_data_out_s
-        );
+    flag_zero: Register1bit port map(
+        clock => clk,
+        rst => rst,
+        wr_en => cu_flags_wr_en_s,
+        data_in => ula_zero,
+        data_out => fr_zero_flag
+    );
 
-    -- MUX de entrada da ULA
-    ula_in_b_s <= reg_data_out_s;
+    flag_carry: Register1bit port map(
+        clock => clk,
+        rst => rst,
+        wr_en => cu_flags_wr_en_s,
+        data_in => ula_carry,
+        data_out => fr_carry_flag
+    );
 
-    -- MUX de entrada do ACC
-    acc_input_s <= reg_data_out_s when sel_acc_src_s = '1' else -- For LOAD instruction
-                   ula_result_s;  
+    -- enables fetch
+    pc_wr_en_s <= '1' when (estado_s = "10" and mmu_exception_s = '0') else '0';
 
-    -- Opcode extraido para o PC
-    opcode_s <= ir_data_out_s(13 downto 9);
+    -- enables decode
+
+    -- enables execute
+    cu_acc_en_s <= acc_en_s when estado_s = "01" else '0';
+    rb_reg_en_s <= cu_reg_wr_en_s when estado_s = "01" else '0';
+
+    -- registers write data mux
+    rb_data_wr_mux_s <= acc_out_s when cu_sel_mux_regs_s = '1' else resize(cu_immediate_s, 16);
+
+    -- acc data input
+    acc_in_s <= rb_data_r_s when (cu_acc_ovwr_en_s = '1' and cu_ram_rd_en_s = '0') else
+            ram_data_out_s when cu_ram_rd_en_s = '1'
+            else unsigned(ula_out_s);
 
 end architecture;
--- src/Processor/processor_script.sh
+++ src/Processor/processor_script.sh
@@ -1,33 +1,22 @@
+#!/bin/bash
+
 rm -rf build
 mkdir build 2>/dev/null
 
-ghdl -a --workdir=build ../StateMachine/state_machine.vhd 
+ghdl -a --workdir=build ../StateMachine/state_machine.vhd
 ghdl -a --workdir=build ../ProgramCounter/program_counter.vhd
 ghdl -a --workdir=build ../ProgramCounter/program_counter_manager.vhd
 ghdl -a --workdir=build ../ROM/rom.vhd
-ghdl -a --workdir=build ../ControlUnit/control_unit.vhd 
+ghdl -a --workdir=build ../RAM/ram.vhd
+ghdl -a --workdir=build ../ControlUnit/control_unit.vhd
 ghdl -a --workdir=build ../ULA/ula.vhd
-ghdl -a --workdir=build ../ULA/acumulador.vhd
-ghdl -a --workdir=build ../banco_reg.vhd
-ghdl -a --workdir=build ../reg_16.vhd
-ghdl -a --workdir=build ../InstructionRegister/instruction_register.vhd
+ghdl -a --workdir=build ../Register1Bit/Register1bit.vhd
+ghdl -a --workdir=build ../RegisterBank/Register16Bits.vhd
+ghdl -a --workdir=build ../RegisterBank/RegisterBank.vhd
+ghdl -a --workdir=build ../MemoryManagementUnit/mmu.vhd
 ghdl -a --workdir=build processor.vhd
 ghdl -a --workdir=build processor_tb.vhd
 
-ghdl -e --workdir=build -o build/state_machine state_machine
-ghdl -e --workdir=build -o build/program_counter program_counter
-ghdl -e --workdir=build -o build/program_counter_manager program_counter_manager
-ghdl -e --workdir=build -o build/rom rom
-ghdl -e --workdir=build -o build/control_unit control_unit
-ghdl -e --workdir=build -o build/ula ula
-ghdl -e --workdir=build -o build/ula acumulador
-ghdl -e --workdir=build banco_reg
-ghdl -e --workdir=build reg_16
-ghdl -e --workdir=build -o build/instruction_register instruction_register
+ghdl --elab-run --workdir=build processor_tb --wave=build/processor_tb.ghw
 
-
-ghdl -e --workdir=build -o processor processor
-ghdl -e --workdir=build -o processor_tb processor_tb
-
-ghdl -r --workdir=build processor_tb --wave=build/processor_tb.ghw
-gtkwave build/processor_tb.ghw -o processor_tb_wave_config.gtkw
+gtkwave build/processor_tb.ghw --save=processor_tb_wave_config.gtkw
